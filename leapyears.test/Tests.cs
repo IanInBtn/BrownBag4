9a3d2b87f21c57845cd37338ba82ad7d72c94048
Ian McArdle (Home) <ian_mcardle@hotmail.com>
2019-11-17 17:50:52 +0000

    Ready to create 1st Leap year test.

--- leapyears.test/Tests.cs
+++ leapyears.test/Tests.cs
@@ -4,13 +4,13 @@ namespace leapyears.test
 {
     public class Tests
     {
-        ////private LeapYearChecker LeapYearChecker { get; set; }
+        private LeapYearChecker LeapYearChecker { get; set; }
 
-        ////[SetUp]
-        ////public void Setup()
-        ////{
-        ////    LeapYearChecker = new LeapYearChecker();
-        ////}
+        [SetUp]
+        public void Setup()
+        {
+            LeapYearChecker = new LeapYearChecker();
+        }
 
         //[Test]
         //public void Test1()
