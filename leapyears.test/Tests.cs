9cbcfd809d21d1ed9b029f3d677fef9797808117
Ian McArdle <ian.mcardle@iqvia.com>
2019-11-21 09:26:16 +0000

    All tests, plus a refactor.

--- leapyears.test/Tests.cs
+++ leapyears.test/Tests.cs
@@ -18,5 +18,23 @@ namespace leapyears.test
         {
             Assert.IsTrue(LeapYearChecker.IsLeapYear(1996));
         }
+
+        [Test]
+        public void IsUntypicalLeapYear()
+        {
+            Assert.IsTrue(LeapYearChecker.IsLeapYear(2000));
+        }
+
+        [Test]
+        public void IsCommonYear()
+        {
+            Assert.IsFalse(LeapYearChecker.IsLeapYear(2001));
+        }
+
+        [Test]
+        public void IsUnTypicalCommonYear()
+        {
+            Assert.IsFalse(LeapYearChecker.IsLeapYear(1900));
+        }
     }
 }
--- leapyears/LeapYearChecker.cs
+++ leapyears/LeapYearChecker.cs
@@ -4,6 +4,25 @@ namespace leapyears
 {
     public class LeapYearChecker
     {
-        public bool? IsLeapYear(int v) => true;
+        public bool? IsLeapYear(int year)
+        {
+            return IsYearUntypicalCommonYear(year) && 
+                   IsYearTypicalLeapYear(year);
+        }
+
+        private static bool IsYearTypicalLeapYear(int year)
+        {
+            return year % 4 == 0;
+        }
+
+        private static bool IsYearUntypicalCommonYear(int year)
+        {
+            if (year % 100 == 0 && year % 400 != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
